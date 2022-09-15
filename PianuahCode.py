85f495732519954f0aa5e9bc928f22086c853ba1
Ricardo Maroko Caster <342688405@gov.il>
2022-09-15 11:38:02 +0300

    PianuahCode_defs_and_params

--- PianuahCode.py
+++ PianuahCode.py
@@ -17,7 +17,7 @@ def read_input():
     df_from_input_excel = pd.read_excel(params["input_excel"], sheet_name="Sheet1")
     df_casenum = pd.DataFrame(columns=['CaseNum'])
     df_casenum['CaseNum'] = pd.DataFrame(df_from_input_excel['מספר מקרה'])
-    print(df_casenum)
+    #print(df_casenum)
     return df_casenum
 
 def create_patnum_and_casenum_df():
@@ -31,26 +31,26 @@ def create_patnum_and_casenum_df():
 
     # reading ALL CaseNums and PatNums from sql table "PRD_DIM_CASES", and storing the CaseNums and PatNums into a python DataFrame
     sql_data = pd.read_sql("SELECT CaseNum, PatNum FROM DWH_PRD..PRD_DIM_CASES", conn) 
-    print("Imported all sql data!!")
+    #print("Imported all sql data!!")
 
     # removing the rows where the CaseNum is not numeric(some casenums starts with "T" for some reason and are not relevant - at least for now)
     sql_data['CaseNum'] = pd.to_numeric(sql_data['CaseNum'], errors='coerce')
     sql_data = sql_data.dropna(subset=['CaseNum'])
 
     #joining casenum df with casenum+patnum df from sql
-    mergedf = pd.merge(df_casenum, sql_data, how='left', on="CaseNum")
-    for index, row in mergedf.iterrows():
-        print(index, row["PatNum"], row["CaseNum"])
+    mergedf = pd.merge(read_input(), sql_data, how='left', on="CaseNum")
+    #for index, row in mergedf.iterrows():
+        #print(index, row["PatNum"], row["CaseNum"])
     
     return mergedf
 
 def create_output_df():
     # Creating an empty Dataframe with column names only
     df_output = pd.DataFrame(columns=['CaseNum', 'PatNum', 'Path_to_pianuah_txt_file'])
-    print(df_output)
+    #print(df_output)
 
     # for each CaseNum in our df, iterarate over files in desired directory(that matches the correct PatNum and CaseNum) and append to a list. Then add new row to the dataframe
-    for index, row in mergedf.iterrows():
+    for index, row in create_patnum_and_casenum_df().iterrows():
         patnum = row["PatNum"]
         casenum = row["CaseNum"]
         path_list = []
@@ -65,14 +65,21 @@ def create_output_df():
             #data = [df_output, temp_df_to_concat]
             #df_output = pd.concat(data)
             ##df_output = df_output.append({'CaseNum': casenum, 'PatNum': patnum, 'Path_to_pianuah_txt_file': path_list}, ignore_index=True)
-    print(df_output)
+    #print(df_output)
     return df_output
 
 def final_csv():
     # Add dataframe data to output csv
-    df_output.to_csv(params["output_csv_file_path"], index=False)
+    create_output_df().to_csv(params["output_csv_file_path"], index=False)
     output_csv_data = pd.read_csv(params["output_csv_file_path"])
     print(output_csv_data.head())
     return output_csv_data
 
+def main():
+    read_input()
+    create_patnum_and_casenum_df()
+    create_output_df()
+    final_csv()
 
+if __name__ == "__main__":
+    main()
